cb9c466af5c5b4d5abd3e450bd2ada677cf8f3d2
tas2580 <tobi.schaefer@gmail.com>
2015-12-08 14:13:11 +0100

    en translation

---
+++ language/en/controller.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* @package phpBB Extension - tas2580 Mobile Notifier
+* @copyright (c) 2015 tas2580 (https://tas2580.net)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'USERMAP_TITLE'						=> 'User Map',
+	'BACK_TO_USERMAP'					=> 'Return to User Map',
+	'DISTANCE'						=> 'Distance',
+	'KM'							=> 'Kilometer',
+	'SET_MY_POSITION'					=> 'Set your own position here',
+	'CLICK_TO_SET'						=> 'Click on the map to set your Position.',
+	'SET_POSITION'						=> 'Set Position',
+	'CONFIRM_COORDINATES_SET'				=> 'Are you sure you wish to set your position to the following coordinates:<br>Longitude %1$s<br> Latitude %2$s',
+	'LON'							=> 'Latitude ',
+	'LAT'							=> 'Longitude',
+	'MENU_CLOSE'						=> 'Close',
+	'MENU_SEARCH'						=> 'Users nearby (%s KM)',
+	'SEARCH_EXPLAIN'					=> 'Users within %1$s Kilometer about %2$s, %3$s',
+	'USERMAP_SEARCH'					=> 'Search User Map',
+	'MEMBERS'						=> 'Members',
+));
